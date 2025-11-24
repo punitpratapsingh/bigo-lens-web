0cf7c9e2e39a0742879396082770ac8a7829ead8
aashi <aashisingh910@gmail.com>
2025-11-24 23:22:56 +0530

    img update

--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -13,7 +13,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 // Asset imports
-import signUpImage from "/src/assets/signUpp.png";
+import signUpImage from "/src/assets/SignUpp.png";
 
 interface SignUpForm {
   companyName: string;
